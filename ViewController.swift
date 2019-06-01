9d2568308a99b8f07849a3b852bac460a4b2f345
Rajat Monga <rajatmonga29@gmail.com>
2019-06-01 18:21:27 -0500

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -1,6 +1,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+     let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font=UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector (handleTextInputChange), for: .editingChanged)
+        return tf
+    }()
+    
+    let usernameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font=UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector (handleTextInputChange), for: .editingChanged)
+        
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.font=UIFont.systemFont(ofSize: 14)
+        tf.addTarget(self, action: #selector (handleTextInputChange), for: .editingChanged)
+        
+        return tf
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
